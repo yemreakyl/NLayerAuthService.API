7f6ee4af571f67d702760354ddb0feb95c9eec49
Yunus emre Akyol <yemreakyl@gmail.com>
2022-05-30 17:46:37 +0300

    custom token classı ve signservice classı eklendi

---
+++ AuthServer.API/SignService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthService.Service
+{
+    static class SignService
+    {
+        //Bu classın amacı üreteceğimiz token larımızı asimetrik yöntemle imzalamak
+        public static SecurityKey GetSecurityKey(string securityKey)
+        {
+            return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+        }
+    }
+}
---
+++ SharedLibrary/Configurations/CustomTokenOption.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Configurations
+{
+    public class CustomTokenOption
+    {
+        //Bu classımın amacı ana apı içindeki appsteetinjson da yer alan token bilgilerine karşılık olarak oluşturdum ve o bilgilerle bu classımı haberdar ediyorum
+        public List<string> Audiences { get; set; } // Token la istek yapılabilecek olan apı lar
+        public string Issuer { get; set; }//Token ı yayınlayan apı
+        public int AccessTokenİnspiration { get; set; }//Geçerlilik süresi
+        public int RefresTokenİnspiration { get; set; }//Geçerlilik süresi
+        public string SecurityKey { get; set; }
+    }
+}
